ab364262271b24b8decf399fc44f33a461efec06
Chinmaya Mohapatra <Chinmaya.Mohapatra@bestbuy.com>
2020-12-17 23:27:07 +0530

    One New File Added

---
+++ HelloJenkins.java
@@ -0,0 +1,8 @@
+class HelloJenkins{
+public static void main(String []ar)
+{
+System.out.println("Welcome to Jenkins Learning");
+}
+
+
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,11 @@ class HelloWorld{
 public static void main(String []ar){
 int a =5;
 int b=5;
-int c = a+b;
+int c=0;
+for(int i =0; i<=5; i++)
+{
+c = a+i;
+}
 System.out.println("This is my First Git Project!"+"  "+"Sum of two project "+c);
 }
 
